3a4ba02e8b17a87fa4eb35b5d8962a56dd28b317
DanielitoAlbinsson <mr.daniel.albinsson@gmail.com>
2016-04-20 16:20:23 +0200

    firstruby exersice

---
+++ my_group.rb
@@ -0,0 +1,7 @@
+
+my_group = Array.new
+
+person_1 = {name: "Daniel", age: 31, eyecollor: "brown"}
+person_2 = {name: "yan", age: 38, eyecollor: "brown"}
+person_3 = {name: "thomas", age: 48, eyecollor: "green"}
+my_group = ["person_1", "person_2", "person_3"]
