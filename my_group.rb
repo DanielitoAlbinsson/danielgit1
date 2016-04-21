3864a1067311567654ac73e7ed3569a6e171b576
DanielitoAlbinsson <mr.daniel.albinsson@gmail.com>
2016-04-21 16:42:34 +0200

    update

--- my_group.rb
+++ my_group.rb
@@ -1,7 +1,8 @@
+person_1 = {name: "Daniel", age: 31, gender: "man"}
+person_2 = {name: "yan", age: 38, gender: "woman"}
+person_3 = {name: "thomas", age: 48, gender: "man"}
+my_group = [person_1, person_2, person_3]
 
-my_group = Array.new
-
-person_1 = {name: "Daniel", age: 31, eyecollor: "brown"}
-person_2 = {name: "yan", age: 38, eyecollor: "brown"}
-person_3 = {name: "thomas", age: 48, eyecollor: "green"}
-my_group = ["person_1", "person_2", "person_3"]
+my_group.each do |per|
+  puts "#{per[:name]} is a #{per[:age]} year old #{per[:gender]}"
+end
